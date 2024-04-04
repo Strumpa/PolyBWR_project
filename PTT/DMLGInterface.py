1738dcfbafca7e2029ca6b4ba06e94eb31baed81
Strumpa <raphael.guasch@polymtl.ca>
2024-04-04 19:59:54 -0400

    worked in Serpent output parser

--- PTT/DMLGInterface.py
+++ PTT/DMLGInterface.py
@@ -15,38 +15,42 @@ from MCNP_card import *
 # Class DMLG_Interface()
 
 class DMLG_Interface:
-    def __init__(self, input_deck, type):
+    def __init__(self, input_deck, type, mode):
         """
         loading geometry object to be parsed according to :
         input file (str) : path to file to be parsed
         type (str) : "MCNP", "Native", "Serpent2"
+        mode (str) : 'input' or 'output'
         """
         self.type = type
         self.input_deck = input_deck
-        if type == "MCNP":
-            self.parseMCNP_deck()
+        self.mode = mode
+        if self.type == "MCNP" and self.mode == 'input' :
+            self.parseMCNP_input()
             self.createMCNPcard_objects()
-        elif type == "Native_Dragon":
-            self.parseNative_deck()
-        elif type == "Serpent2":
-            self.parseSerpent2_deck()
+        elif self.type == "Native_Dragon" and self.mode == 'input' :
+            self.parseNative_input()
+        elif self.type == "Serpent2" and self.mode == 'input' :
+            self.parseSerpent2_input()
+        elif self.type == "Serpent2" and self.mode == 'output' : 
+            self.parseSerpent2_output()
         else:
             print("Error: invalid Geometry type to be parsed")
         
         
-    def parseMCNP_deck(self):
+    def parseMCNP_input(self):
         """
         parsing the MCNP input file
         Its main structure contains a defintion of "Cell Cards", "Surface Cards" and "Material Cards" 
-        Each section has a dictionnary associated in order to store the info about each card.
-        The dictionnarys are :
+        Each section has a list associated in order to store the info about each card.
+        The lists are :
         for Cell Cards : group of cells with associated data
         for Surface Cards : group of surfaces with asociated data
         for Material Cards : material name with associated data
 
         Classes defining the proper data structure for each card/ group of cards are called in this function.
         """
-        print(f"Parsing geometry of type : {self.type}, from input file : {self.input_deck}")
+        print(f"Parsing {self.type} {self.mode} file, from input file : {self.input_deck}")
         # Define flags to track when to start/stop parsing each section
         parsing_cell_cards = False
         parsing_surface_cards = False
@@ -70,17 +74,14 @@ class DMLG_Interface:
 
                 # Check if the line indicates the start of a section
                 if "Cell  Cards" in line:
-                    #print("in cell cards")
                     parsing_cell_cards = True
                     parsing_surface_cards = False
                     parsing_material_cards = False
                 elif "Surface Cards" in line:
-                    #print("in surface cards")
                     parsing_cell_cards = False
                     parsing_surface_cards = True
                     parsing_material_cards = False
                 elif "Material  Cards" in line:
-                    #print("in material cards")
                     parsing_cell_cards = False
                     parsing_surface_cards = False
                     parsing_material_cards = True
@@ -91,23 +92,17 @@ class DMLG_Interface:
                 
                 elif parsing_cell_cards or parsing_surface_cards:
                     if line.startswith("c"):
-                        #print("in current title")
                         if line.lstrip("c").strip():
                             current_title=line.lstrip("c").strip()
-                    #print(current_title)
 
                 # Parse and store information from each section
                 elif parsing_cell_cards or parsing_surface_cards and line:
-                    #print("in parsing")
                     # Store previous card's contents (if any) in the corresponding dictionary
                     if parsing_cell_cards:
-                        #print("in parsing cell cards")
                         cell_cards.append(line.replace("  ", " ").replace("   "," "))
                         cell_titles.append(current_title)
                     elif parsing_surface_cards:
-                        #print("in parsing surface cards")
                         surface_cards.append(line.replace("  ", " ").replace("   "," "))
-                        print(current_title)
                         surface_titles.append(current_title)
                 elif parsing_material_cards and line:
                     if line.startswith("m"):
@@ -117,7 +112,7 @@ class DMLG_Interface:
                     if not line.startswith("c"):
                         line=line.replace("  ", " ").replace("   "," ")
                         material_cards.append(line)
-                        
+        file.close()              
 
  
 
@@ -127,15 +122,12 @@ class DMLG_Interface:
         self.surface_titles = self.clean_list(surface_titles)
         
         self.Material_Cards = self.combine_list(material_cards) 
-        #print(self.Material_Cards)
-        #self.Material_Cards = self.clean_list(material_cards)
-        #self.material_titles = self.clean_list(material_titles)
         
     def clean_list(self,list_):
         cleaned_list=[]
         for entry in list_:
-            if entry != '':
-                cleaned_list.append(entry)
+            if entry != '' or entry !="\n":
+                cleaned_list.append(entry.strip())
         return cleaned_list
     def combine_list(self, list_):
         combined_list=[]
@@ -176,23 +168,83 @@ class DMLG_Interface:
                 print(f"The corresonding surface equation is : {surface_card.surfaceEquation}")
         if print_materials:
             print(f"There are a total of {len(self.Material_Cards)} Material cards")
+        
+        return
 
 
 
 
 
-    def parseNative_deck(self):
+    def parseNative_input(self):
         print("Native geom type not suported yet")
         return
 
-    def parseSerpent2_deck(self):
+    def parseSerpent2_input(self):
         print("Serpent2 geom type not supported yet")
         return
+    def parseSerpent2_output(self):
+        """
+        parsing Serpent2 output file
 
+       
+        """
+        lines=[]
+        materials=[]
+        material_data_={}
+        current_material=None
+        parsing_new_mat = False
+        parsing_same_mat = False
+        print(f"Parsing {self.type} {self.mode} file, from input file : {self.input_deck}")
+        with open(self.input_deck, 'r') as file:
+            for line in file:
+                line = line.strip()  # Remove leading/trailing whitespace
+                #print(line)
+                if "Material" in line and ":" in line:
+                    current_material=line.split(" ")[1].strip('"').strip(":").strip('"')
+                    materials.append(current_material)
+                    parsing_new_mat=True
+                #print(current_material)
+                #print(current_material in material_data_.keys())
+                elif parsing_new_mat:
+                    #print("Parsing new line")
+                    #print(line)
+                    material_data_[current_material] = line
+                    parsing_new_mat=False
+                    parsing_same_mat=True
+                elif parsing_same_mat:
+                    if "density" in line :
+                        data = material_data_[current_material] + line.split(' ')[3]+" "
+                    elif "Volume" in line or "Mass" in line :
+                        data = material_data_[current_material] + line.split(' ')[2]+" "
+                    elif ".06c" in line or ".05c" in line or ".03c" in line:
+                        data = material_data_[current_material] + line.split('  ')[0]+" "+line.split('  ')[4]+" "
+                    else:
+                        data = material_data_[current_material]
+                    #print("combinin new line with preivous data")
+                    #print(data)
+                    material_data_.pop(current_material)
+                    material_data_[current_material]=data
+                """
+                if current_material in material_data_.keys():
+                    data=material_data_[current_material]+line
+                    material_data_.pop(current_material)
+                    material_data_[current_material]=data
+                elif line:
+                    material_data_[current_material]=line
+                """
+        file.close()
+        print(materials)
+        print(material_data_)
 
+        return
+
+"""
 input_f = "MCNP_AT10_sanitized.inp"
-AT10_CRTL_MCNP = DMLG_Interface(input_f, type="MCNP")
+AT10_CRTL_MCNP = DMLG_Interface(input_f, type="MCNP",mode="input")
 #print(AT10_CRTL_MCNP.Cell_Cards)
 AT10_CRTL_MCNP.getMCNP_card_data(print_cells=False, print_surfaces=False, print_materials=True)
 #print(AT10_CRTL_MCNP.Cell_Cards)
 #print(AT10_CRTL_MCNP.Cell_Cards['water box centered at ( 8.267, 6.973)'].material_densities[2])
+"""
+cell_output = "Serpent2/data_AT10_24UOX_try/AT10_24UOX_mc.out"
+AT10_24UOX_cell = DMLG_Interface(cell_output, type="Serpent2",mode="output")
---
+++ PTT/Serpent2_cards.py
@@ -0,0 +1,10 @@
+# Serpent2 card class structures in Python3
+#  
+# Instances of this class are initialized in the DMLG_Interface class when parsing Serpent2 cards
+# Author: R. Guasch
+# Part of the DMLG code Package.
+#
+# Contribution to BWR geometry handling for the Version5 environment.
+# 
+# Structure of the Cell card object obtained from "MCNP® USER’S MANUAL Code Version 6.2 October 27, 2017"
+# 
