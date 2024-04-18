e062776bd0ba12d89d6c36106fb3e64dc31805e8
Strumpa <raphael.guasch@gmail.com>
2024-04-17 22:34:07 -0400

    worked in error plotting S2

--- PTT/Serpent_POSTPROC.py
+++ PTT/Serpent_POSTPROC.py
@@ -41,9 +41,9 @@ def load_serpent2_keffs(path_to_serpent2_res, case_name):
     serpent_keff=res.resdata["absKeff"]
     np.savetxt(SAVE_DIR+f'serpent_keff_{case_name}.txt',serpent_keff)
     SERPENT_keff=np.loadtxt(SAVE_DIR+f'serpent_keff_{case_name}.txt',dtype=float)
-    SERPENT_Keff=np.zeros(lenBU_SERPENT)
     Ls2=np.shape(SERPENT_keff)
-    lenBU_SERPENT=Ls2[0]    
+    lenBU_SERPENT=Ls2[0]
+    SERPENT_Keff=np.zeros(lenBU_SERPENT)    
     for k in range(lenBU_SERPENT):
         SERPENT_Keff[k]=SERPENT_keff[k][0] # reshaping array to a 1D list
     return SERPENT_Keff
@@ -82,8 +82,9 @@ def load_Serpent_IsoDens(path_to_serpent2_res, case_name, isotope):
 def plot_comparisons_keff(listBU, listsKeff, case_names, dpi, save_dir): # case_names[0] is the reference
     print('$$$ -------- POSTPROC.py : Serpent2 figures (Keff) ')
     fig,ax = plt.subplots(dpi=dpi)
+    print(case_names)
     for i in range(len(case_names)):
-        ax.plot(listBU, listsKeff[i], label=f"Keff for {case_names[i]}")
+        ax.plot(listBU, listsKeff[i], label=case_names[i])
     ax.legend()
     ax.set_xlabel("BU (MWj/t)")
     ax.set_ylabel("Keff Serpent2")
@@ -94,18 +95,39 @@ def plot_comparisons_isotopeDens(listBU, listsIsoDens, case_names, isotope, dpi,
     print('$$$ -------- POSTPROC.py : Serpent2 figures (Iso dens) ')
     fig,ax = plt.subplots(dpi=dpi)
     for i in range(len(case_names)):
-        ax.plot(listBU, listsIsoDens[i], label=f"Concentration of {isotope} for {case_names[i]}")
+        ax.plot(listBU, listsIsoDens[i], label=f"Concentration of {isotope} for "+case_names[i])
     ax.legend()
     ax.set_xlabel("BU (MWj/t)")
     ax.set_ylabel(f"Isotope density of {isotope} (a/b*cm)")
     ax.set_title(f"Comparison of {isotope} density for PyNjoy2016 tests")
     fig.savefig(save_dir+f"Evolution_{isotope}_comparison")
 
+def compute_errors_to_ref(keff_ref, listKeffs_tests):
+    errors_list = []
+    for test_keffs in listKeffs_tests:
+        tmp_err =[]
+        for i in range(len(test_keffs)):
+            tmp_err.append((1/keff_ref[i]-1/test_keffs[i])*1e5)
+        errors_list.append(tmp_err)
+    return errors_list
+
+def plot_error_to_ref(listBU, errors_keff, labels, save_dir):
+    fig,ax = plt.subplots(dpi=250)
+    for i in range(len(errors_keff)):
+        ax.plot(listBU, errors_keff[i], labels[i])
+    ax.legend()
+    ax.set_xlabel("BU (MWj/t)")
+    ax.set_ylabel(f"Error on Keff (pcm)")
+    ax.set_title(f"Comparison of errors for PyNjoy2016 tests")
+    fig.savefig(save_dir+f"Evolution_{isotope}_comparison")
 
 reference_keffs=load_serpent2_keffs(path_to_ref, ref_name)
 test1_keffs=load_serpent2_keffs(path_to_test1, test1_name)
 
+print(test1_keffs)
+
 serpBU=load_Serpent_BU(path_to_ref, ref_name)
+print(serpBU)
 
 isotopes_data_ref = []
 isotopes_data_test1 =[]
@@ -113,8 +135,10 @@ for iso in isotopes_:
     isotopes_data_ref.append(load_Serpent_IsoDens(path_to_ref, ref_name, iso))
     isotopes_data_test1.append(load_Serpent_IsoDens(path_to_test1, test1_name, iso))
 
-plot_comparisons_keff(serpBU,[reference_keffs, test1_name], ["AT10 24UOX, reference Serpent JEFF311", "AT10 24UOX, test 1, modified PyNjoy2016"],dpi=250, save_dir=SAVE_DIR)
+errors_keff = compute_errors_to_ref(reference_keffs, [test1_keffs])
+
+plot_comparisons_keff(serpBU,[reference_keffs, test1_keffs], ["AT10 24UOX, reference Serpent JEFF311", "AT10 24UOX, test 1, modified PyNjoy2016"],dpi=250, save_dir=SAVE_DIR)
 for iso_num in range(len(isotopes_)):
-    plot_comparisons_isotopeDens(serpBU, [isotopes_data_ref[iso_num], isotopes_data_test1[iso_num]], ["AT10 24UOX, reference", "AT10 24UOX, test 1, modified PyNjoy2016"], dpi=250, save_dir=SAVE_DIR)
+    plot_comparisons_isotopeDens(serpBU, [isotopes_data_ref[iso_num], isotopes_data_test1[iso_num]], ["AT10 24UOX, reference", "AT10 24UOX, test 1, modified PyNjoy2016"], isotopes_[iso_num],dpi=250, save_dir=SAVE_DIR)
 
 
