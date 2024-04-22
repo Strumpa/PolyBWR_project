5592163e01de84f59113b707f85c5a3d725fc333
Strumpa <raphael.guasch@polymtl.ca>
2024-04-22 16:30:11 -0400

    worked on THM, case1 with fill_between option to plot

--- THM_prototype/THM_MONO.py
+++ THM_prototype/THM_MONO.py
@@ -16,37 +16,47 @@ class FDM_HeatConductionInFuelPin:
         self.I_f = I_f # number of mesh elements in the fuel
         self.gap_w = gap_width # gap width in meters, used to determine mesh elements for constant surface discretization
         self.clad_w = clad_width # clad width in meters, used to determine mesh elements for constant surface discretization
+
+        self.gap_r = self.r_f+self.gap_w
+        self.clad_r = self.gap_r + self.clad_w
+
+
         self.I_c = I_c # number of mesh elements in clad
         self.Qfiss = Qfiss # Fission power density in W/m^3
         self.Hg = Hgap # Heat transfer coefficient through gap W/m^2/K
         self.z = z # corresponding height in m corresponding to axial discretization used in FVM_ConvectionInCanal class
         self.T_surf = T_surf # Boundary condition outer clad surface temperature computed from FVM_ConvectionInCanal class 
-        
+        self.kf = kf
+        self.kc = kc
         # compute relevant quantities to initialise object
         self.N_node = I_f + I_c +2
         self.A = np.eye(self.N_node)
         self.D = np.zeros(self.N_node)
-        self.compute_radii()
         self.compute_Area_meshes()
-        self.radii_at_centers = np.sqrt(self.A_mesh_centers*2)
-        self.radii_at_bounds = np.sqrt(self.A_mesh_bounds*2)
-        self.kf = kf
-        self.kc = kc
+        self.compute_radii()
+        self.initialize_ks()
+
+        self.initialise_plotting_mesh("m")
+        self.physical_regions_bounds = [0, self.r_f, self.gap_r, self.clad_r]
+        
+
+        
+        
+        
+    def initialize_ks(self):
         # this array is probaby not needed here as one might assume that kf and kc
         # are constant in fuel/clad. I wanted to keep and option to let them vary according to temperature at node as conductive properties might differ when high temperature gradients are present.
-        self.k = np.zeros(self.N_node-1) # associate a k to each mesh center element
+        self.k = np.zeros(self.N_node-1) # associate a k to the center of each mesh element
         for i in range(len(self.k)):
             if i <self.I_f:
-                self.k[i]=kf
+                self.k[i]=self.kf
             elif i == self.I_f:
                 self.k[i]=0 # in gap!
             elif i<self.N_node:
-                self.k[i]=kc
+                self.k[i]=self.kc
         return
-    def compute_radii(self):
-        self.gap_r = self.r_f+self.gap_w
-        self.clad_r = self.gap_r + self.clad_w
-        return
+
+
     def compute_Area_meshes(self):
         """
         building necessary meshes for solving the heat conduction equation on the constant area discretization 
@@ -57,22 +67,31 @@ class FDM_HeatConductionInFuelPin:
         A_f = self.r_f**2/2
         A_gf =self.gap_r**2/2
         A_cgf = self.clad_r**2/2
+        print(f"Area of fuel = {A_f}")
+        print(f"Area of fuel+gap = {A_gf}")
+        print(f"Area of fuel+gap+clad = {A_cgf}")
         self.deltaA_f = A_f / self.I_f # base assumption is that delta_A is constant in each region --> delta A fuel = constant in fuel, delta A clad = constant in clad and 1 delta A gap.
+        print(f"Delta_Af = {self.deltaA_f}")
         for i in range(self.I_f+1):
             self.A_mesh_bounds.append(i*self.deltaA_f)
         for i in range(self.I_f):
             self.A_mesh_centers.append(i*self.deltaA_f+self.deltaA_f/2)
-            %self.A_calculation_mesh.append(i*self.deltaA_f+self.deltaA_f/2)
+            #self.A_calculation_mesh.append(i*self.deltaA_f+self.deltaA_f/2)
     
         self.deltaA_g = A_gf-A_f
+        print(f"Delta_A in gap = {self.deltaA_g}")
         self.A_mesh_bounds.append(self.A_mesh_bounds[-1]+self.deltaA_g)
         self.A_mesh_centers.append(self.A_mesh_centers[-1]+self.deltaA_f/2+self.deltaA_g/2) # last center in fuel + half of the fuel area step to get to the last fuel bound + half of the gap area step to get to the center of the gap
         self.deltaA_c = (A_cgf-A_gf)/self.I_c
+        print(f"Delta_A in cald is ={self.deltaA_c}")
         for i in range(self.I_c):
             self.A_mesh_bounds.append(self.A_mesh_bounds[-1]+self.deltaA_c)
         for i in range(self.I_c):
-            self.A_mesh_centers.append(self.A_mesh_centers[-1]+self.deltaA_c)
-            self.A_calculation_mesh.append(self.A_mesh_centers[-1]+self.deltaA_c)
+            if i==0:
+                self.A_mesh_centers.append(self.A_mesh_centers[-1]+self.deltaA_c/2+self.deltaA_g/2)
+            else:
+                self.A_mesh_centers.append(self.A_mesh_centers[-1]+self.deltaA_c)
+            #self.A_calculation_mesh.append(self.A_mesh_centers[-1]+self.deltaA_c)
         self.A_mesh_centers = np.array(self.A_mesh_centers)
         self.A_mesh_bounds = np.array(self.A_mesh_bounds)
         #self.A_calculation_mesh = np.array(self.A_calculation_mesh)
@@ -85,8 +104,16 @@ class FDM_HeatConductionInFuelPin:
             elif i == self.I_f+1:
                 self.A_calculation_mesh[i] = A_f + self.deltaA_g
             elif i > self.I_f+1:
-                self.A_calculation_mesh[i] = self.A_calculation_mesh[i-1] + i*self.deltaA_c
+                print(f"last elem in calculation mesh is {self.A_calculation_mesh[i-1]} ==> {np.sqrt(2*self.A_calculation_mesh[i-1])} m")
+                print(f"index used is {(i-(self.I_f+2))}")
+                self.A_calculation_mesh[i] = A_gf + self.deltaA_c/2 + (i-(self.I_f+2))*self.deltaA_c
+                print(f"A_gf = {A_gf}, r_gf = {np.sqrt(2*A_gf)}")
 
+        return
+    
+    def compute_radii(self):
+        self.radii_at_centers = np.sqrt(self.A_mesh_centers*2)
+        self.radii_at_bounds = np.sqrt(self.A_mesh_bounds*2)
         return
     
     def get_Di_half(self,i):
@@ -145,8 +172,52 @@ class FDM_HeatConductionInFuelPin:
     
     def solve_T_in_pin(self):
         self.T_distrib = np.linalg.solve(self.A, self.D)
-     
+        self.compute_T_center()
+        T_distrib_with_center = np.zeros(self.N_node+1)
+        T_distrib_with_center[0] = self.T_center
+        for i in range(1,self.N_node+1):
+            T_distrib_with_center[i] = self.T_distrib[i-1]
+        self.T_distrib = T_distrib_with_center
+
+    def compute_T_center(self):
+        # using equation (13) of "Revisiting the simplified thermo-hydraulics module THM: in DONJON5 code" - A. Hébert, March 2018 to compute T_(3/2).
+        T_3_2 = (self.deltaA_f*self.k[0]*self.T_distrib[0]+self.deltaA_f*self.k[1]*self.T_distrib[1])/(self.deltaA_f*self.k[0]+self.deltaA_f*self.k[1])
+        # using equation (46) of "Revisiting the simplified thermo-hydraulics module THM: in DONJON5 code" - A. Hébert, March 2018 to compute T_center.
+        self.T_center = 2*self.T_distrib[0] - T_3_2
+        print(f"Computed T_3/2 is {T_3_2} K, T_1 = {self.T_distrib[0]} K, T_2 = {self.T_distrib[1]} K.")
+        print(f"Computed T_center (at r=0) = {self.T_center} K.")
+        return
+    def compute_T_eff(self):
+        # using equation (45) of "Revisiting the simplified thermo-hydraulics module THM: in DONJON5 code" - A. Hébert, March 2018 to compute T_eff
+        # This corresponds to the simplified correlation, the so-called Rowlands formula :
+        self.T_eff = (5/9)*self.T_surf + (4/9)*self.T_center
+        return
     
+    def initialise_plotting_mesh(self, unit):
+        """
+        unit = "m" or "mm"
+        this only affects the visualization/plotting units. However all input quantities have to be in MKS units to be consistent with the solver. 
+        """
+        # building plot mesh in radial units taking the central temperature into account.
+        self.plot_mesh = np.zeros(self.N_node+1)
+        self.plot_mesh[0] = 0
+        self.plotting_units = unit
+        for i in range(1,self.N_node+1):
+            if unit == "m":
+                self.plot_mesh[i] = np.sqrt(2*self.A_calculation_mesh[i-1])
+            elif unit == "mm":
+                self.plot_mesh[i] = np.sqrt(2*self.A_calculation_mesh[i-1])*1e3
+    
+    def extend_to_canal_visu(self, rw, Tw):
+        A_w = rw**2/2
+        deltA_w = A_w - self.A_mesh_bounds[-1] 
+        w_center = np.sqrt(2*(A_w + deltA_w/2)) 
+        self.plot_mesh = np.append(self.plot_mesh,[w_center])
+        self.physical_regions_bounds.append(rw)        
+        self.T_distrib = np.append(self.T_distrib, [Tw])
+        self.radii_at_bounds = np.append(self.radii_at_bounds, [rw])
+
+        
 class FVM_ConvectionInCanal:
     def __init__(self, Lf, T_in, Q_flow, P_cool, I_z, canal_type, rf, rc, rw):
         """
