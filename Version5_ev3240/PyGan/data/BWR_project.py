e8d01262e0f4e28e43bf1b8bac58ac098fcb0c8a
Strumpa <raphael.guasch@gmail.com>
2024-05-29 17:48:15 -0400

    worked on Gd/RSE scheme, started validation work for 2x2 and 3x3 UOX+Gd

--- Version5_ev3240/PyGan/data/BWR_project.py
+++ Version5_ev3240/PyGan/data/BWR_project.py
@@ -75,8 +75,8 @@ TYPE = 'HYBRIDE'
 # - assemblies : 
 #           - 'ATRIUM-10XM_NL24-2'
 #           - 'ATRIUM-10XM_NL24-2_ctrl'
-name_geom = 'AT10_UOX' # 'AT10_UOX' # 'AT10_UOX_Gd2O3'
-name_mix = "AT10_24UOX" #"AT10_24UOX" "AT10_45Gd"
+name_geom = 'AT10_UOX_Gd2O3' # 'AT10_UOX' # 'AT10_UOX_Gd2O3'
+name_mix = "AT10_45Gd" #"AT10_24UOX" "AT10_45Gd"
 tracking_module = "SALT" #"SALT"
 #
 # Multicompo = 1 if you want to generate a MULTICOMPO object 
@@ -89,7 +89,7 @@ Multicompo = 1
 #        - 'UOx'  : used for UOx fuel without Gd poison
 #        - 'Gd'   : used for UOx fuel with Gd poison
 #        - 'free' : modify the burnup points as you wish
-burnup_points = 'UOx4_autop5'  # 'UOx4_autop5' 'UOx2_autop5' 'Gd_autop5'
+burnup_points = 'Gd_autop5'  # 'UOx4_autop5' 'UOx2_autop5' 'Gd_autop5'
 # suffixe = suffixe added to name_geom for creation of figures, MULTICOMPO and BU vector
 suffixe = tracking_module+"_"+burnup_points
 #
