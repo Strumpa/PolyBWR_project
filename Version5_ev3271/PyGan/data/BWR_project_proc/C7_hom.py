cf93033ba3fd4d79072a4a37b28335e12db10199
Strumpa <raphael.guasch@polymtl.ca>
2024-06-09 18:30:38 -0400

    setting up pygan homoegeneous calculation

---
+++ Version5_ev3271/PyGan/data/BWR_project_proc/C7_hom.py
@@ -0,0 +1,28 @@
+#####################################################################
+#                                                                   #
+# Description : PyGan scritp for BWR simulation with DRAGON5        #
+# Author      : R. Guasch                                           #
+# Date        : 2023/2024                                           #
+# Purpose     : Depletion calculation for homogeneous for pin cell  #
+#                                                                   #
+#####################################################################
+#
+import lifo
+import cle2000
+
+def C7_hom(namCOMPO,name_compo):
+
+  # Lifo
+  myLifo=lifo.new()
+  myLifo.pushEmpty(namCOMPO,"LCM")
+  myLifo.push(name_compo)
+  myLifo.lib()
+
+  # Execution 
+  calcBWR = cle2000.new('C7_hom',myLifo,1)
+  calcBWR.exec()
+
+  # Recover
+  myLifo.lib()
+  pyCOMPO = myLifo.node(namCOMPO)
+  return pyCOMPO
